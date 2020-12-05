e0394828d2f0cd1d912eff39d4d0856b8f3c876a
Archana Bhor <archana.bhor@gmail.com>
2020-12-05 17:27:14 -0500

    DB: Moved img colum from User table to candidate and organization table. Changed Seed file accordingly

--- server/db/models/candidate.js
+++ server/db/models/candidate.js
@@ -2,38 +2,42 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Candidate = db.define('candidate', {
-    name: {
-        type: Sequelize.STRING,
-        allownull: false,
-        validate: {
-            notEmpty: true
-        }
+  name: {
+    type: Sequelize.STRING,
+    allownull: false,
+    validate: {
+      notEmpty: true,
     },
-    description: {
-        type: Sequelize.TEXT
+  },
+  img: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  location: {
+    type: Sequelize.STRING,
+    allownull: false,
+    validate: {
+      notEmpty: true,
     },
-    location: {
-        type: Sequelize.STRING,
-        allownull: false,
-        validate: {
-            notEmpty: true
-        }
+  },
+  isRemote: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true,
+  },
+  currentRole: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true,
     },
-    isRemote: {
-     type: Sequelize.BOOLEAN,
-     defaultValue: true
-    },
-    currentRole: {
-        type: Sequelize.STRING,
-        validate: {
-            notEmpty: true
-        }
-    },
-    currentCompany: {
-        type: Sequelize.STRING,
-        allowNull: true
-    }
-
+  },
+  currentCompany: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
 })
 
 module.exports = Candidate
