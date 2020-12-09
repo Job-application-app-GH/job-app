4bdc57c55fa169632d81b2d8d9a0c505f5ad488e
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-12-09 14:40:57 -0600

    upload works

--- server/api/upload.js
+++ server/api/upload.js
@@ -20,39 +20,31 @@ router.post('/', async (req, res, next) => {
     })
     let secureURL = uploadResponse.secure_url
     let user = req.user
-    // let updatedprofile
-    let findUser = await Candidate.findOne({
-      where: {
-        userId: req.user.id,
-      },
-    })
-    console.log('user usertype: ', user.userType)
-    let updatedprofile = await Candidate.update(
-      {img: secureURL},
-      {
-        where: {
-          userId: req.user.id,
-        },
-        returning: true,
-        plain: true,
-      }
-    )
-    // }
-    // else {
-    //   updatedprofile = await Organization.update(
-    //     {img: secureURL},
-    //     {
-    //       where: {
-    //         userId: req.user.id,
-    //       },
-    //       returning: true,
-    //       plain: true,
-    //     }
-    //   )
-    // }
+    let updatedprofile
+    if (user.userType === 'CANDIDATE') {
+      updatedprofile = await Candidate.update(
+        {img: secureURL},
+        {
+          where: {
+            userId: req.user.id,
+          },
+          returning: true,
+          plain: true,
+        }
+      )
+    } else {
+      updatedprofile = await Organization.update(
+        {img: secureURL},
+        {
+          where: {
+            userId: req.user.id,
+          },
+          returning: true,
+          plain: true,
+        }
+      )
+    }
     console.log('updated profile', updatedprofile)
-    // console.log('url--->', secureURL)
-    // console.log('updated Profile[0]-->', updatedprofile[0])
     res.send(updatedprofile)
   } catch (error) {
     next(error)
