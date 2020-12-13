9ba1c4b799b54ded534ed72f8d90e09a6b47dc89
Mackenzie Kroon <kroonmackenzie@gmail.com>
2020-12-12 21:30:01 -0600

    org matches and match profile

--- src/components/JobMatchCandidateProfile.js
+++ src/components/JobMatchCandidateProfile.js
@@ -38,44 +38,47 @@ class JobMatchCandidateProfile extends React.Component {
     const skills = this.props.skillsList
     console.log('email->', candidateEmail)
     return (
-          <div className='global-screen-box'>
+      <div className="global-screen-box">
         <OrgHeader />
-    
-        <h3>Want to reach out? </h3>
-        {candidateEmail ? (
-          <button>
-            <a href={'mailto:' + candidateEmail}>
-              Send {profile.name} an email
-            </a>
+        <div className="profile-matches-container">
+          <h3>Want to reach out? </h3>
+          {candidateEmail ? (
+            <button className="profile-edit-org-button">
+              <a href={'mailto:' + candidateEmail}>
+                Send {profile.name} an email
+              </a>
+            </button>
+          ) : null}
+
+          <h2>{profile.name}</h2>
+
+          <h3>Location: {profile.location}</h3>
+          {profile.currentCompany ? (
+            <h3>Current Company: {profile.currentCompany}</h3>
+          ) : null}
+          {profile.currentRole ? (
+            <h3>Current Role: {profile.currentRole}</h3>
+          ) : null}
+          <h3>Description: {profile.description}</h3>
+          {profile.isRemote ? (
+            <h3>Willing to hire remote candidates: True</h3>
+          ) : (
+            <h3>Willing to hire remote candidates: False</h3>
+          )}
+          <h3>Skills</h3>
+          {skills
+            ? // <h5>Skills:</h5>
+              skills.map((skill) => (
+                <div key={skill.id}>
+                  <p>{skill.name}</p>
+                </div>
+              ))
+            : null}
+
+          <button style={{backgroundColor: 'white'}} onClick={this.goBack}>
+            BACK TO MATCHES
           </button>
-        ) : null}
-
-        <h2>{profile.name}</h2>
-
-        <h6>Location: {profile.location}</h6>
-        {profile.currentCompany ? (
-          <h6>Current Company: {profile.currentCompany}</h6>
-        ) : null}
-        {profile.currentRole ? (
-          <h6>Current Role: {profile.currentRole}</h6>
-        ) : null}
-        <p>Description: {profile.description}</p>
-        {profile.isRemote ? (
-          <h6>Willing to hire remote candidates: True</h6>
-        ) : (
-          <h6>Willing to hire remote candidates: False</h6>
-        )}
-        <h5>Skills</h5>
-        {skills
-          ? // <h5>Skills:</h5>
-            skills.map((skill) => (
-              <div key={skill.id}>
-                <p>{skill.name}</p>
-              </div>
-            ))
-          : null}
-
-        <button onClick={this.goBack}>BACK TO MATCHES</button>
+        </div>
       </div>
     )
   }
